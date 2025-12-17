0067dab2b9772c8ff909495ebd148952af665bee
bala-amypo <bala@amypo.in>
2025-12-17 08:07:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/LocationContoller.java
@@ -0,0 +1,12 @@
+@RestContrller
+public class LocationController{
+  @Autowired
+  LocationService LocationService
+  @PostMapping("/addstudent")
+  public LocalEntity add(@RequestBody LocalEntity le){
+    
+  }
+
+
+
+}
