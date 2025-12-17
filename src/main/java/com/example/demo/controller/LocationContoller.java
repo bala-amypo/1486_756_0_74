235625dea742e55bfedc0713124e767c230f9337
bala-amypo <bala@amypo.in>
2025-12-17 08:17:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/LocationContoller.java
+++ src/main/java/com/example/demo/controller/LocationContoller.java
@@ -1,3 +1,14 @@
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+
+import com.example.model.LocalEntity;
+import com.example.service.LocationService;
+
 @RestContrller
 public class LocationController{
   @Autowired
